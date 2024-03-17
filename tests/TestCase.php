8eb849f894304b747915ed15760b83704c1f4fbe
bee-interactive <bee-interactive@users.noreply.github.com>
2024-03-17 15:20:34 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,9 @@
 
 namespace BeeInteractive\LaravelNotificationsReminders\Tests;
 
+use BeeInteractive\LaravelNotificationsReminders\LaravelNotificationsRemindersServiceProvider;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Orchestra\Testbench\TestCase as Orchestra;
-use BeeInteractive\LaravelNotificationsReminders\LaravelNotificationsRemindersServiceProvider;
 
 class TestCase extends Orchestra
 {
